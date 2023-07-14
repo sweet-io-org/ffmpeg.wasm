b9e73501a942b496198ce56b29f91ba5b8e538db
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2023-07-13 22:15:37 -0400

    change path

--- src/browser/defaultOptions.js
+++ src/browser/defaultOptions.js
@@ -5,7 +5,8 @@ const { devDependencies } = require('../../package.json');
  * Default options for browser environment
  */
 module.exports = {
-  corePath: '../../ffmpeg-core/ffmpeg-core.js'
+  // corePath: '../../ffmpeg-core/ffmpeg-core.js'
+  corePath: 'https://nhl-static.assets.playground.sweet.io/ffmpeg-core/ffmpeg-core.js'
   //  (typeof process !== 'undefined' && process.env.FFMPEG_ENV === 'development')
   //   ? resolveURL('/node_modules/@ffmpeg/core/dist/ffmpeg-core.js')
   //   : `https://unpkg.com/@ffmpeg/core@${devDependencies['@ffmpeg/core'].substring(1)}/dist/ffmpeg-core.js`,
