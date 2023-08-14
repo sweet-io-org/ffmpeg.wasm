5afefa1666657fb014ebf0ccdd95b3b64bde7330
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2023-08-14 11:35:43 -0400

    Update defaultOptions.js

--- src/browser/defaultOptions.js
+++ src/browser/defaultOptions.js
@@ -6,7 +6,7 @@ const { devDependencies } = require('../../package.json');
  */
 module.exports = {
   // corePath: '../../ffmpeg-core/ffmpeg-core.js'
-  corePath: 'https://nhl-static.assets.playground.sweet.io/ffmpeg-core/ffmpeg-core.js'
+  corePath: 'https://nhl-static.assets.sweet.io/ffmpeg-core/ffmpeg-core.js'
   //  (typeof process !== 'undefined' && process.env.FFMPEG_ENV === 'development')
   //   ? resolveURL('/node_modules/@ffmpeg/core/dist/ffmpeg-core.js')
   //   : `https://unpkg.com/@ffmpeg/core@${devDependencies['@ffmpeg/core'].substring(1)}/dist/ffmpeg-core.js`,
